8a1d076e793d06c5c1cde49d9b0edd476f269664
Mazy <mazy_ios@163.com>
2020-07-12 10:08:37 +0800

    init newtonCradle

---
+++ NewtonCradle/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NewtonCradle
+//
+//  Created by Mazy on 2020/7/12.
+//  Copyright © 2020 Mazy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
